cc8bcd5c3000009a3fa50fbbb1294150c2c01ae4
dmitry <Buza.dima93@gmail.com>
2020-08-11 13:08:29 +0600

    SPM added

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "XLPagerTabStrip",
+    products: [
+        .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"])
+    ],
+    targets: [
+        .target(
+            name: "XLPagerTabStrip",
+            path: "Sources",
+            exclude: ["FXPageControl.h", "FXPageControl.m"]
+        )
+    ]
+)
